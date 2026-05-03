2c34e5bd92cdaf07ff30a0756ecba506eae33d1a
Vishal-Mernstack <vishalkumarmahto.bcr23181@imsnoida.com>
2026-05-03 23:00:34 +0530

    Push day1_basics.py

---
+++ day1_basics.py
@@ -0,0 +1,10 @@
+name =input ("enter your name: ");
+age =input ("enter you age:");
+
+age=int(age);
+
+print(f"hello {name}, you are {age} years old.");
+
+future_age= age + 5
+
+print(f" In 5 years, you will be {future_age} years old.");
